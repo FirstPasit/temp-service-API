ef3329a3a0e7e1c171835f4b3db7bfb60611a2d8
FirstPasit <giccer11@gmail.com>
2023-10-27 10:58:40 +0700

    Refactor index.js : adding /temp cal

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/conv/:temp/:unit', function(req, res, ) {
+  let tem = Number(req.params.temp);
+  let unit = req.params.unit;
+  let temp_conv;
+  let unit_conv;
+  if(unit.trim() == 'cel'){
+    temp_conv = ((tem/5)*9)+32;
+    unit_conv = "Fahrenheit";
+  }else{//fahrenheit
+    temp_conv = ((tem -32)/9)*5;
+    unit_conv = "Celcius";
+  }
+  let result = {"temp_conv":temp_conv,"unit_conv":unit_conv};
+  res.send(result);
+});
+
 module.exports = router;
